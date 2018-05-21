5ec42073339be105092a4ffe65eccd94d4412c3b
Syméon Carle <symeoncarle@iMac-de-Symeon.local>
2018-05-21 23:32:10 +0200

    Première version de la recheche qui fontionne

---
+++ PolarDictionary/PolarDictionary/CustomUIElements/UIPickerView/SelecteurOptionnelSurEntité.swift
@@ -0,0 +1,21 @@
+//
+//  SelecteurOptionnelSurEntité.swift
+//  PolarDictionary
+//
+//  Created by Syméon Carle on 21/05/2018.
+//  Copyright © 2018 PIDR. All rights reserved.
+//
+
+import UIKit
+
+class SelecteurOptionnelSurEntite_: SelecteurSurEntité {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
